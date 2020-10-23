bf2f4a154e4211baadeab0d4e89b67aece2844a6
Matthew <matvey.w2015@mail.ru>
2020-10-23 15:07:10 +0400

    add cssBaseLine component from material ui

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import React, { StrictMode } from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { ThemeProvider } from '@material-ui/core/styles';
+import { ThemeProvider,CssBaseline } from '@material-ui/core';
 import theme from './theme';
 import App from './App';
 import './index.scss';
@@ -9,6 +9,7 @@ import './index.scss';
 render(
   <StrictMode>
     <ThemeProvider theme={theme}>
+      <CssBaseline/>
       <Router>
         <App />
       </Router>
