16e0a3a241b08c2ba825514f061286bcce5fdd4a
Matvey <matvey.w2015@mail.ru>
2020-10-30 18:22:16 +0400

    refactor tweet component

--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -1,118 +1,209 @@
 import React from 'react';
 import {Tweet} from "../components/Tweet";
 import {
-    Grid,
-    makeStyles,
-    Typography,
     Container,
-    InputBase,
-    withStyles,
     createStyles,
-    Paper, Theme,
+    Grid,
+    InputBase,
+    makeStyles,
+    Paper, TextField,
+    Theme,
+    Typography,
+    withStyles,
 } from '@material-ui/core';
 import grey from '@material-ui/core/colors/grey'
 import {SideMenu} from "../components/SideMenu";
 
 export const useHomeStyles = makeStyles((theme: Theme) => ({
-    wrapper: {
-        height: '100vh',
-    },
-    logo: {
-        margin: '10px 0',
-    },
-    logoIcon: {
-        fontSize: 36,
-    },
-    sideNavList: {
-        width: 230,
-        listStyle: 'none',
-        padding: 0,
-        margin: 0,
-    },
-    sideNavListItem: {
-        display: 'inline-block',
-        cursor: 'pointer',
-        '&:hover div': {
-            backgroundColor: 'rgba(29,161,242,0.1)',
-            '& h6': {
-                color: theme.palette.primary.main,
+        wrapper: {
+            height: '100vh',
+        },
+        logo: {
+            margin: '10px 0',
+        },
+        logoIcon: {
+            fontSize: 36,
+        },
+        sideNavList: {
+            position: 'sticky',
+            top: 0,
+            maxWidth: 230,
+            listStyle: 'none',
+            padding: 0,
+            margin: 0,
+        },
+        sideNavListItem: {
+            display: 'inline-block',
+            cursor: 'pointer',
+            [theme.breakpoints.down('sm')]: {
+                display: 'block',
             },
-            '& svg path': {
-                fill: theme.palette.primary.main,
+            '&:hover div': {
+                backgroundColor: 'rgba(29,161,242,0.1)',
+                '& h6': {
+                    color: theme.palette.primary.main,
+                },
+                '& svg path': {
+                    fill: theme.palette.primary.main,
+                },
+            },
+            '& div': {
+                display: 'inline-flex',
+                alignItems: 'center',
+                padding: '0 20px 0 15px',
+                borderRadius: 30,
+                marginBottom: 15,
+                height: 50,
+                transition: 'all 250ms ease',
+            },
+            '&:last-of-type': {
+                display: 'block',
             },
         },
-        '& div': {
-            display: 'inline-flex',
-            alignItems: 'center',
-            padding: '0 20px 0 15px',
-            borderRadius: 30,
-            height: 50,
-            transition: 'all 250ms ease',
+        sideNavListItemLabel: {
+            fontSize: 20,
+            fontWeight: 700,
+            marginLeft: 8,
         },
-        '&:last-of-type': {
-            display: 'block',
+        sideNavListItemIcon: {
+            fontSize: 32,
         },
-    },
-    sideNavListItemLabel: {
-        fontSize: 20,
-        fontWeight: 700,
-        marginLeft: 8,
-    },
-    sideNavListItemIcon: {
-        fontSize: 32,
-    },
-    sideNavTweetButton: {
-        padding: theme.spacing(3),
-        marginTop: theme.spacing(2)
-    },
-    tweetsWrapper: {
-        borderRadius: 0,
-        height: '100%',
-        borderTop: '0',
-        borderBottom: '0',
-    },
-    tweetsHeader: {
-        borderTop: '0',
-        borderLeft: '0',
-        borderRight: '0',
-        borderRadius: 0,
-        padding: '15px 15px',
+        sideNavTweetButton: {
+            padding: theme.spacing(3),
+            marginTop: theme.spacing(2)
+        },
+        tweetsWrapper: {
+            borderRadius: 0,
+            height: '100%',
+            borderTop: '0',
+            borderBottom: '0',
+        },
+        tweetsHeader: {
+            borderTop: '0',
+            borderLeft: '0',
+            borderRight: '0',
+            borderRadius: 0,
+            padding: '15px 15px',
 
-        '& h6': {
-            fontWeight: 800
+            '& h6': {
+                fontWeight: 800
+            },
         },
-    },
-    tweet: {
-        cursor: 'pointer',
-        paddingTop: 15,
-        paddingLeft: 20,
-        '&:hover': {
-            backgroundColor: 'rgb(245,248,250)'
+        tweet: {
+            display: 'flex',
+            paddingTop: 15,
+            paddingLeft: 20,
+            cursor: 'pointer',
+            '&:hover': {
+                backgroundColor: 'rgb(245,248,250)'
+            },
         },
-    },
-    tweetAvatar: {
-        width: theme.spacing(5),
-        height: theme.spacing(5),
-    },
-    tweetFooter: {
-        display: 'flex',
-        position: 'relative',
-        left: -10,
-        justifyContent: 'space-between',
-        width: 450,
-    },
-    tweetFooterIcon: {
-        '&:hover': {
-            backgroundColor: 'rgba(29,161,242,0.1)'
+        tweetAvatar: {
+            width: theme.spacing(5),
+            height: theme.spacing(5),
+            marginRight: 15,
         },
-        '&:hover .tweetFooterIcon': {
-            fill: 'rgba(29,161,242,0.1)'
-        }
-    },
-    tweetUserName: {
-        color: grey[500]
-    },
-}));
+        tweetFooter: {
+            display: 'flex',
+            position: 'relative',
+            left: -10,
+            justifyContent: 'space-between',
+            maxWidth: 450,
+        },
+        tweetFooterIcon: {
+            '&:hover': {
+                backgroundColor: 'rgba(29,161,242,0.1)'
+            },
+            '&:hover .tweetFooterIcon': {
+                fill: 'rgba(29,161,242,0.1)'
+            }
+        },
+        tweetUserName: {
+            color: grey[500]
+        },
+        widgetSide: {
+            position: 'sticky',
+            top: 0,
+            paddingTop: 20,
+        },
+        widgetSideBlock: {
+            backgroundColor: '#F5F8FA',
+            borderRadius: 15,
+            marginTop: 20,
+            '& .MuiList-root': {
+                paddingTop: 0,
+            },
+        },
+        widgetSideBlockHeader: {
+            borderTop: 0,
+            borderLeft: 0,
+            borderRight: 0,
+            backgroundColor: 'transparent',
+            padding: '13px 18px',
+            '& b': {
+                fontSize: 20,
+                fontWeight: 800,
+            },
+        },
+        widgetSideBlockItem: {
+            cursor: 'pointer',
+            '& .MuiTypography-body1': {
+                fontWeight: 700,
+            },
+            '& .MuiListItemAvatar-root': {
+                minWidth: 50,
+            },
+            '& .MuiListItemText-root': {
+                margin: 0,
+            },
+            "&:hover": {
+                backgroundColor: '#edf3f6',
+            },
+        },
+        addForm: {
+            padding: 20,
+        },
+        addFormBody: {
+            display: 'flex',
+            width: '100%',
+        },
+        addFormBottom: {
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+        },
+        addFormBottomActions: {
+            marginTop: 10,
+            paddingLeft: 70,
+        },
+        addFormTextarea: {
+            width: '100%',
+            border: 0,
+            fontSize: 20,
+            outlineWidth: '0',
+            fontFamily: 'inherit',
+            resize: 'none',
+        },
+        addFormBottomLine: {
+            height: 12,
+            backgroundColor: '#E6ECF0',
+        },
+        addFormCircleProgress: {
+            position: 'relative',
+            width: 20,
+            height: 20,
+            margin: '0 10px',
+            '& .MuiCircularProgress-root': {
+                position: 'absolute',
+            },
+        },
+        addFormBottomRight: {
+            display: 'flex',
+            alignItems: 'center',
+        },
+    }
+));
+
 
 const SearchTextField = withStyles(() => createStyles({
     input: {
